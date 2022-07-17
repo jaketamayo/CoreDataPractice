eea5ca728200f3c93b6a9f37f4ae89ad88bee5de
Jake Tamayo <hellojtmatthew@icloud.com>
2022-07-17 16:52:34 -0500

    Initial Commit

---
+++ CoreDataPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataPractice
+//
+//  Created by Jacob Tamayo on 7/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataPractice/CoreDataPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPracticeApp.swift
+//  CoreDataPractice
+//
+//  Created by Jacob Tamayo on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
